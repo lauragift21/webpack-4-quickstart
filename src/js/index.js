3915491bb8769dcb71a28d8e42c1d8b955a40e66
lauragift21 <laurandidi21@gmail.com>
2018-10-07 07:54:44 +0100

    complete redux app

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
-import store from '../js/store/index';
-import { addArticle } from '../js/actions/index';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/index';
+import App from './components/App';
 
-
-window.store = store;
-window.addArticle = addArticle;
+// wrap the provider to give the store access to the application
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
